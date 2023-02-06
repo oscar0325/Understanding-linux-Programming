6d0f3008dc4161a7248f2d5b10909d7f199c1a49
oscar0325 <yangjunfeng0325@163.com>
2023-02-06 21:30:39 +0800

    230206

---
+++ ch4/spwd.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+ino_t get_inode(char *);
+void printpathto(ino_t);
+void inum_to_name(ino_t, char *, int);
+
+int main()
+{
+    printpathto(get_inode("."));
+    putchar('\n');
+    return 0;
+}
+
+void printpathto(ino_t this_inode)
+/*
+prints path leading down to an object with this inode
+kindof recursive
+*/
+{
+    ino_t my_inode;
+    char its_name[BUFSIZ];
+    if(get_inode("..") != this_inode)
+    {
+        chdir("..");
+        inum_to_name(this_inode, its_name, BUFSIZ);
+        my_inode = get_inode(".");
+        printpathto(my_inode);
+        printf("/%s", its_name); //从根目录开始打印
+    }
+
+}
+
+void inum_to_name(ino_t inode_to_find, char *namebuf, int buflen)
+{
+    DIR *dir_ptr;
+    struct dirent *dirent;
+    dir_ptr = opendir(".");
+    if(dir_ptr == NULL)
+    {   
+        perror(".");
+        exit(1);
+    }
+
+    while((dirent = readdir(dir_ptr)) != NULL)
+    {
+        if(dirent -> d_ino == inode_to_find)
+        {
+            strncpy(namebuf, dirent -> d_name, buflen);
+            namebuf[buflen - 1] = '\0';
+            closedir(dir_ptr);
+            return;
+        }
+    }
+    fprintf(stderr,"error looking for inum %ld\n", inode_to_find);
+    exit(1);
+}
+
+
+ino_t get_inode(char *fname)
+{
+    struct stat info;
+    if(stat(fname, &info) == -1)
+    {
+        fprintf(stderr, "Cannot stat ");
+        perror(fname);
+        exit(1);
+    }
+    return info.st_ino;
+}
